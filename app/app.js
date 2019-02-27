838340969cc4f2b279ace1edb7dbe11cdb98a738
appelzd <appel.david@live.com>
2019-02-26 20:01:06 -0700

    adding axios and webassembly

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
-(function(){
+(function(){    
 
+document.getElementById('fileUpload').addEventListener("change", e => {
+    let reader = new FileReader();
+    reader.readAsDataURL(e.target['files'][0]);
+})
 
-
-    if('serviceWorker' in navigator){
-        navigator.serviceWorker.register('app/cache-service-worker.js').then(() => console.log('added cache service worker'));
-    }
+    //https://codelabs.developers.google.com/codelabs/your-first-pwapp/index.html?index=..%2F..index#5
+    // if('serviceWorker' in navigator){
+    //     navigator.serviceWorker.register('cache-service-worker.js').then(() => console.log('added cache service worker'));
+    // }
 
 })();
 
