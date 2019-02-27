0d0951bc545b256cec55d30390cb7ca7f067e2ca
appelzd <appel.david@live.com>
2019-02-27 10:17:57 -0700

    adding test framework

---
+++ app/GoogleVision/googleVisionProxy.spec.js
@@ -0,0 +1,11 @@
+
+import { googleVisionProxy } from './googleVisionProxy';
+
+const proxy = googleVisionProxy;
+describe("test proxy loads",
+    it("null test", () => expect(proxy).not(null))
+);
+
+
+
+
